55d7754b3efab073416f95595004559c2b346f8e
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-05-16 20:55:01 -0300

    Ordenamiento de sql, se agrega sql que crea la relacion colectivos_por_parada

---
+++ sql/01-sql_tp_colectivos_creacion_tablas.sql
@@ -0,0 +1,109 @@
+-- cargo tabla de colectivos por parada
+DROP TABLE IF EXISTS colectivos_por_parada;
+
+create table colectivos_por_parada (
+	id_parada integer, 
+	linea_colectivo integer
+);
+
+COPY colectivos_por_parada
+FROM 'C:\Users\kspasiuk\Documents\base_de_datos\tp_colectivos\fuentes\paradas-de-colectivo-parseado.csv'
+DELIMITER ';'
+CSV HEADER         --para indicar que la primera l ́ınea es el encabezado
+ENCODING 'LATIN1';
+
+-- cargo tabla info de recorridos
+DROP TABLE IF EXISTS info_recorridos;
+
+create table info_recorridos(
+	WKT text not null,
+	ID integer not null,
+	LINEA integer,
+	TIPO_SERVICIO text,
+	RAMAL text,
+	SENTIDO text
+);
+
+COPY info_recorridos
+FROM 'C:\Users\kspasiuk\Documents\GitHub\base_de_datos\fuentes\recorrido-colectivos.csv'
+DELIMITER ';'
+CSV HEADER         
+ENCODING 'LATIN1';
+
+-- creo tabla lineas 
+DROP TABLE IF EXISTS lineas;
+
+create table lineas as
+select distinct 
+	linea,
+	ramal,
+	sentido
+from info_recorridos;
+
+ALTER TABLE lineas ADD CONSTRAINT id_linea_ramal_sentido PRIMARY KEY (linea,ramal,sentido);
+
+-- cargo tabla de info de paradas. 
+DROP TABLE IF EXISTS info_paradas;
+
+CREATE TABLE info_paradas (
+	x double precision,
+	y double precision,
+	stop_id integer not null,
+	tipo text,
+	calle text,
+	numero text,
+	entre1 text,
+	entre2 text,
+	lineas text,
+	dir_nor text,
+	calle_nor text,
+	ALTURA_NOR integer,
+	COORDX text,
+	COORDY text,
+	METROBUS text,
+	STOP_NAME text,
+	STOP_DESC text,
+	FUENTE text,
+	VERIFICADA text,
+	FECHA_ULTI date
+);
+
+COPY info_paradas
+FROM 'C:\Users\kspasiuk\Documents\base_de_datos\tp_colectivos\fuentes\paradas-de-colectivo.csv'
+DELIMITER ';'
+CSV HEADER         --para indicar que la primera l ́ınea es el encabezado
+ENCODING 'LATIN1';
+
+-- creo tabla paradas 
+DROP TABLE IF EXISTS paradas;
+
+create table paradas as 
+select 
+	stop_id as id_parada,
+	calle,
+	numero as altura,
+	st_setsrid(st_makepoint(x,y),4326) as point_posicion
+from info_paradas
+where (calle is not null and numero is not null and x is not null and y is not null);
+
+--agrego clave primaria en paradas
+ALTER TABLE paradas ADD CONSTRAINT id_parada PRIMARY KEY (id_parada);
+
+
+-- creo tabla posiciones 
+DROP TABLE IF EXISTS posiciones;
+
+create table posiciones as 
+select distinct 
+	x as longitud,
+	y as latitud,
+	st_setsrid(st_makepoint(x,y),4326) as point_posicion
+from info_paradas 
+where x is not null and y is not null; 
+
+-- agrego clave primaria en posiciones
+ALTER TABLE posiciones ADD CONSTRAINT id_posicion PRIMARY KEY (point_posicion);
+
+-- agrego clave foraneas en paradas
+ALTER TABLE paradas ADD CONSTRAINT id_posicion FOREIGN KEY (point_posicion) REFERENCES posiciones;
+
---
+++ sql/03-creacion_tabla_colectivos_por_parada.sql
@@ -0,0 +1,53 @@
+
+-- correr despúes despues de carga_reocrrido_colectivos_lucas.sql
+drop table if exists recorrido_colectivos;
+create table recorrido_colectivos as
+select 
+	linea::integer,
+	sentido,
+	recorrido,
+	st_linemerge(geom) as geom
+from lineasnac_polyline ;
+
+-- creo la tabla de colectivos_por_parada 
+
+-- creo tabla temporal donde cuenta la distancia entre el recorrido y la parada
+
+create table tmp_t1 as (
+	select 
+	t1.id_parada,
+	t1.linea_colectivo,
+	sentido,
+	recorrido,
+	ST_Distance(t2.point_posicion,t3.geom) as distance
+from colectivos_por_parada t1
+inner join paradas t2 on (t1.id_parada = t2.id_parada)
+inner join recorrido_colectivos t3 on (t1.linea_colectivo = t3.linea)
+order by id_parada, linea_colectivo);
+
+-- genero la tabla de colectivos por paradas, quedandome con el sentido que tenga menor distancia. 
+
+drop table colectivos_por_parada;
+
+create table colectivos_por_parada as(
+select t1.id_parada,
+	 t1.linea_colectivo,
+	 t1.sentido,
+	 t1.recorrido
+from tmp_t1 t1 
+inner join (
+	select
+		id_parada,
+		linea_colectivo,
+		min(distance) as min_distance
+	from tmp_t1 t1
+	group by
+	id_parada,
+	linea_colectivo) t2 on t2.id_parada = t1.id_parada and t1.linea_colectivo = t2.linea_colectivo and
+	t2.min_distance = t1.distance);
+
+
+drop table tmp_t1;
+drop table info_paradas;
+drop table info_recorridos;
+drop table lineasnac_polyline;
