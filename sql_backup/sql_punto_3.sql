95fa2ffb25baf131d57fc0b018ce2207418c9286
edjus <justo.edson@gmail.com>
2019-06-02 11:21:47 -0300

    se agrega nueva version a la solución del punto 3

--- sql_backup/sql_punto_3.sql
+++ sql_backup/sql_punto_3.sql
@@ -1,3 +1,4 @@
+-- Version con EXISTS - EXCEPT
 WITH paradas_colectivo AS (
 	SELECT DISTINCT id_parada, recorrido
 	FROM  colectivos_por_parada
@@ -14,3 +15,22 @@ INNER JOIN (SELECT DISTINCT pc1.id_parada
 			   						WHERE pc1.id_parada = pc3.id_parada)
 									)
 			) AS R on pc.id_parada = R.id_parada
+
+
+-- version con EXISTS - NOT EXISTS
+WITH paradas_colectivo AS (
+	SELECT DISTINCT id_parada, recorrido
+	FROM  colectivos_por_parada
+	WHERE linea_colectivo = ['Numero de colectivo']
+)
+SELECT p.calle, pc.id_parada, pc.recorrido
+FROM paradas p
+INNER JOIN paradas_colectivo pc ON pc.id_parada = p.id_parada
+INNER JOIN (SELECT DISTINCT pc.id_parada 
+			FROM paradas_colectivo pc
+			WHERE EXISTS((SELECT 1 FROM paradas_colectivo pc2
+						WHERE NOT EXISTS (SELECT 1 FROM paradas_colectivo pc3
+											WHERE pc.id_parada = pc3.id_parada 
+										  	and pc2.recorrido = pc3.recorrido))
+)) AS R on pc.id_parada = R.id_parada
+			
